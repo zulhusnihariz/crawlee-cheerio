eeb92a2765c7deb0940f4105a510f9aa0fc8fd36
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-11-10 03:07:23 +0800

    remove p img filter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -47,60 +47,60 @@ router.addDefaultHandler(async ({ $, log, request }) => {
     case HOSTNAME.AMBCRYPTO:
       data.content = $('div.single-post-main-middle').children('p').text().trim()
       data.description = $('span.single-post-excerpt p').text().trim()
-      data.htmlContent = $('div.single-post-main-middle').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.single-post-main-middle').children('p').toString()
       break
     case HOSTNAME.BEINCRYPTO: {
       data.content = $('div.entry-content-inner').children('p').text().trim()
-      data.htmlContent = $('div.entry-content-inner').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.entry-content-inner').children('p').toString()
       break
     }
     case HOSTNAME.BIT_COLUMNIST: {
       data.content = $('div.post-content').children('p').text()
       data.updatedAt = $('span.date-modified time[datetime]').attr('datetime') ?? ''
-      data.htmlContent = $('div.post-content').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.post-content').children('p').toString()
       break
     }
     case HOSTNAME.BLOCKONOMI: {
       data.content = $('div.post-content').children('p').text()
-      data.htmlContent = $('div.post-content').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.post-content').children('p').toString()
       break
     }
     case HOSTNAME.COIN_DESK: {
       data.content = $('.at-body p').text().trim()
       data.description = $('.at-subheadline h2').text().trim()
       data.updatedAt = $('.at-updated span').text().trim()
-      data.htmlContent = $('.at-body p:not(:has(img))').toString()
+      data.htmlContent = $('.at-body p').toString()
       break
     }
     case HOSTNAME.COIN_SPEAKER: {
       data.content = $('div.content').children('p').text()
       data.description = $('div.content_excerpt p').text().trim()
-      data.htmlContent = $('div.content').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.content').children('p').toString()
       break
     }
     case HOSTNAME.COIN_TELEGRAPH: {
       data.content = $('.post-content').children('p').text()
       data.description = $('p.post__lead').text().trim()
-      data.htmlContent = $('.post-content').children('p:not(:has(img))').toString()
+      data.htmlContent = $('.post-content').children('p').toString()
       break
     }
     case HOSTNAME.CRYPTO_NEWSZ: {
       data.content = $('div.entry-content').children('p').text()
-      data.htmlContent = $('div.entry-content').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.entry-content').children('p').toString()
       break
     }
     case HOSTNAME.CRYPTO_POTATO: {
       data.author = $('.entry-user a').text().trim()
       data.content = $('div.coincodex-content').children('p').text()
       data.createdAt = $('.entry-date span.last-modified-timestamp').text().trim()
-      data.htmlContent = $('div.coincodex-content').children('p:not(:has(img))').toString()
+      data.htmlContent = $('div.coincodex-content').children('p').toString()
 
       if (data.createdAt) data.createdAt = data.createdAt.replace('@', '')
       break
     }
     case HOSTNAME.CRYPTO_SLATE: {
       data.content = $('article.full-article').children('p').text()
-      data.htmlContent = $('article.full-article').children('p:not(:has(img))').toString()
+      data.htmlContent = $('article.full-article').children('p').toString()
       break
     }
     case HOSTNAME.DAILY_COIN: {
